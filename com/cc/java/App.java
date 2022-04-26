3e1ccc311cd30e0eb1352a9c6930e93f41012ca0
MariuszPS <k8kpol@gmail.com>
2022-04-26 10:56:52 +0200

    ok

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,8 +1,31 @@
 package com.cc.java;
 
+import java.util.Arrays;
+
 public class App {
     
     public static void main(String[] args) {
+
+            //Deklaration : int
+          int[] intArr = new int [4];
+          output("Arr: "+ intArr);
+          output("Array-Länge: "+ intArr.length);
+          output("1.Element: "+ intArr[0]);
+
+          intArr[0] = 1;
+          intArr[1] = 2;
+          intArr[2] = 3;
+          intArr[3] = 4;
+          intArr[4] = 10;   //Index ausserhalb der Grenze!!!
+
+
+          output("1.Element: "+ intArr[0]);
+          output("2.Element: "+ intArr[1]);
+          output("Letztes Element: "+ intArr[3]);
+          output("Letztes Element: "+ intArr[intArr.length-1]);
+
+
+
      
     }
 
